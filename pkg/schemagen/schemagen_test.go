70ce286896a26a88cc77a36bcab6da5f78d686ad
Sebastian Widmer <sebastian.widmer@vshn.net>
2026-08-07 14:54:12 +0200

    Test generated CRDs by applying some examples

--- pkg/schemagen/schemagen_test.go
+++ pkg/schemagen/schemagen_test.go
@@ -3,12 +3,15 @@ package schemagen_test
 import (
 	"io/fs"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"helm.sh/helm/v4/pkg/chart/v2/loader"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	kubeyaml "sigs.k8s.io/yaml"
 
@@ -19,6 +22,7 @@ import (
 func TestGenerateCRD_Golden(t *testing.T) {
 	scheme, restCfg := testutil.SetupEnvtestEnv(t)
 	cli, err := client.New(restCfg, client.Options{Scheme: scheme})
+	cli = client.WithFieldValidation(cli, client.FieldValidation("Strict"))
 	require.NoError(t, err, "Creating controller-runtime client failed")
 
 	charts, err := fs.Glob(os.DirFS("testdata/charts"), "*/values.yaml")
@@ -53,6 +57,39 @@ func TestGenerateCRD_Golden(t *testing.T) {
 				require.NoError(t, kubeyaml.UnmarshalStrict(yamlData, &crd), "Failed to unmarshal golden CRD YAML for chart: %s", chartPath)
 
 				require.NoError(t, cli.Create(t.Context(), &crd), "Failed to apply golden CRD for chart: %s", chartPath)
+				defer func() {
+					require.NoError(t, cli.Delete(t.Context(), &crd), "Failed to delete golden CRD for chart: %s", chartPath)
+				}()
+				require.Eventually(t, func() bool {
+					var tmpCRD apiextv1.CustomResourceDefinition
+					if err := cli.Get(t.Context(), client.ObjectKey{Name: crd.Name}, &tmpCRD); err != nil {
+						t.Logf("Failed to get CRD %s: %s", crd.Name, err.Error())
+						return false
+					}
+					return len(tmpCRD.Status.AcceptedNames.Plural) > 0
+				}, 3*time.Second, 10*time.Millisecond)
+
+				applytests, err := fs.Glob(os.DirFS(filepath.Join("testdata/charts", chartPath)), "examples/*.yaml")
+				require.NoError(t, err)
+				for _, applytest := range applytests {
+					t.Run("Test examples "+applytest, func(t *testing.T) {
+						applytestPath := filepath.Join("testdata/charts", chartPath, applytest)
+						yamlData, err := os.ReadFile(applytestPath)
+						require.NoError(t, err, "Failed to read apply test YAML for chart: %s", chartPath)
+
+						var obj unstructured.Unstructured
+						require.NoError(t, kubeyaml.UnmarshalStrict(yamlData, &obj), "Failed to unmarshal apply test YAML for chart: %s", chartPath)
+						obj.SetNamespace("default")
+						matchError := obj.GetAnnotations()["match-error"]
+						createErr := cli.Create(t.Context(), &obj)
+
+						if matchError != "" {
+							require.ErrorContains(t, createErr, matchError)
+						} else {
+							require.NoError(t, createErr)
+						}
+					})
+				}
 			})
 		})
 	}
